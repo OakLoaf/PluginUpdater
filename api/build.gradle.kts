e5288a71346cdd6ea95c618f5a0c515858d9af0b
OakLoaf <oak@beaconstudios.org>
2025-09-17 18:24:42 +0100

    Corrected dependency scope

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,7 +7,8 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.21.8-R0.1-SNAPSHOT")
 
     implementation("org.lushplugins:ChatColorHandler:5.1.6")
-    implementation("org.jetbrains:annotations:26.0.2")
+
+    compileOnlyApi("org.jetbrains:annotations:26.0.2")
 }
 
 java {
