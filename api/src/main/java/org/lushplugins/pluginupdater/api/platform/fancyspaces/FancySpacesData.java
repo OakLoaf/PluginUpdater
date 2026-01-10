fb7ca92e1eff8786827bc548d87208b8753d1e57
Thomas Keig <keigthomas@gmail.com>
2026-01-10 02:21:44 +0000

    Add constructor to FancySpacesData for config section

--- api/src/main/java/org/lushplugins/pluginupdater/api/platform/fancyspaces/FancySpacesData.java
+++ api/src/main/java/org/lushplugins/pluginupdater/api/platform/fancyspaces/FancySpacesData.java
@@ -1,11 +1,20 @@
 package org.lushplugins.pluginupdater.api.platform.fancyspaces;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.lushplugins.pluginupdater.api.platform.PlatformData;
 
 public class FancySpacesData implements PlatformData {
+    private static final String NAME = "fancyspaces";
+
     private final String spaceId;
 
+    public FancySpacesData(ConfigurationSection configurationSection) {
+        super(NAME);
+        this.spaceId = configurationSection.getString("space-id");
+    }
+
     public FancySpacesData(String spaceId) {
+        super(NAME);
         this.spaceId = spaceId;
     }
 
