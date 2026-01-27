d4144e32e7b167f757307cd02ff2a65aa5ab0d81
Oak <55620516+OakLoaf@users.noreply.github.com>
2026-01-27 18:47:27 +0000

    Update plugin versions in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ import java.io.InputStreamReader
 plugins {
     `java-library`
     `maven-publish`
-    id("com.gradleup.shadow") version("8.3.0")
-    id("xyz.jpenilla.run-paper") version("2.3.1")
+    id("com.gradleup.shadow") version("9.3.1")
+    id("xyz.jpenilla.run-paper") version("3.0.2")
     id("com.modrinth.minotaur") version ("2.+")
 }
 
