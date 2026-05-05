edacb748cccfbf72bb0d75cb7c8e08287779d5af
Oak <oak@beaconstudios.org>
2026-05-05 23:49:04 +0300

    Finalised maven publishing

--- common/api/build.gradle.kts
+++ common/api/build.gradle.kts
@@ -18,14 +18,3 @@ dependencies {
     api("com.google.code.gson:gson:2.14.0")
     compileOnlyApi("org.jetbrains:annotations:26.1.0")
 }
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = rootProject.group.toString() + "." + rootProject.name.lowercase()
-            artifactId = "common-api"
-            version = rootProject.version.toString()
-            from(project.components["java"])
-        }
-    }
-}
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,7 +3,7 @@ subprojects {
         publications {
             create<MavenPublication>("maven") {
                 groupId = rootProject.group.toString() + "." + rootProject.name.lowercase()
-                artifactId = "updater." + project.name
+                artifactId = "updater.common-" + project.name
                 version = rootProject.version.toString()
                 from(project.components["java"])
             }
