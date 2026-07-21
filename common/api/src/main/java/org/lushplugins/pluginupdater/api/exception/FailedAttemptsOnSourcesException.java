c8a89bb4daabda6a52fc1c32facbaf923f3e1915
Oak <oak@beaconstudios.org>
2026-07-21 12:12:18 +0100

    Removed unused code

--- common/api/src/main/java/org/lushplugins/pluginupdater/api/exception/FailedAttemptsOnSourcesException.java
+++ common/api/src/main/java/org/lushplugins/pluginupdater/api/exception/FailedAttemptsOnSourcesException.java
@@ -5,8 +5,4 @@ public class FailedAttemptsOnSourcesException extends Exception {
     public FailedAttemptsOnSourcesException(String message) {
         super(message, null, false, false);
     }
-
-    public FailedAttemptsOnSourcesException(String message, Throwable cause) {
-        super(message, cause, false, false); // TODO: Check whether stack trace needs to be writable here
-    }
 }
