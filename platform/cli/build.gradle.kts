509e70b1264f796b5f1bc7bc6a9db5b34471e40a
Oak <oak@beaconstudios.org>
2026-07-30 16:22:38 +0100

    Adjusted dependency scope

--- platform/cli/build.gradle.kts
+++ platform/cli/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
-    implementation(project(":common:impl"))
+    api(project(":common:impl"))
     implementation("io.github.revxrsal:lamp.cli:4.0.0-rc.17")
-    implementation("net.kyori:adventure-text-minimessage:5.2.0")
+    api("net.kyori:adventure-text-minimessage:5.2.0")
     api("com.electronwill.night-config:json:3.9.0")
 }
