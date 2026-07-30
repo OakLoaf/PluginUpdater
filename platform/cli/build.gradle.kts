9b7a3d7599419a065c1d81b432fdd60d14cb895c
Oak <oak@beaconstudios.org>
2026-07-30 16:20:04 +0100

    Removed unneeded dependency

--- platform/cli/build.gradle.kts
+++ platform/cli/build.gradle.kts
@@ -1,7 +1,6 @@
 dependencies {
     implementation(project(":common:impl"))
     implementation("io.github.revxrsal:lamp.cli:4.0.0-rc.17")
-//    implementation("net.kyori:adventure-api:5.2.0") // TODO: Check whether this dependency is needed in addition to MiniMessage
     implementation("net.kyori:adventure-text-minimessage:5.2.0")
     api("com.electronwill.night-config:json:3.9.0")
 }
