cee6c38d16b9b2183fb52008e3ec4ee1b8017f74
Oak <oak@beaconstudios.org>
2026-07-31 21:39:16 +0100

    Added system properties to platform tests

--- platform/paper-plugin/src/test/java/org/lushplugins/pluginupdater/paper/test/PaperUpdaterTests.java
+++ platform/paper-plugin/src/test/java/org/lushplugins/pluginupdater/paper/test/PaperUpdaterTests.java
@@ -5,6 +5,11 @@ import org.lushplugins.pluginupdater.tests.commonplugins.CommonPluginsTest;
 
 public class PaperUpdaterTests {
 
+    static {
+        System.setProperty("platform", "paper");
+        System.setProperty("server-version", "26.1.2");
+    }
+
     @Test
     public void test() {
         CommonPluginsTest.runTest("common-plugins.yml");
--- platform/velocity-plugin/src/test/java/org/lushplugins/pluginupdater/paper/test/VelocityUpdaterTests.java
+++ platform/velocity-plugin/src/test/java/org/lushplugins/pluginupdater/paper/test/VelocityUpdaterTests.java
@@ -5,6 +5,11 @@ import org.lushplugins.pluginupdater.tests.commonplugins.CommonPluginsTest;
 
 public class VelocityUpdaterTests {
 
+    static {
+        System.setProperty("platform", "velocity");
+        System.setProperty("server-version", "4.1.0");
+    }
+
     @Test
     public void test() {
         CommonPluginsTest.runTest("common-plugins.yml");
