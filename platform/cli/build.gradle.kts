e804f43c11dbb82ecc3c336be4226d0c0884d62d
Oak <55620516+OakLoaf@users.noreply.github.com>
2026-07-31 14:23:12 +0100

    Made CLI runnable via jar

--- platform/cli/build.gradle.kts
+++ platform/cli/build.gradle.kts
@@ -4,3 +4,9 @@ dependencies {
     api("net.kyori:adventure-text-minimessage:5.2.0")
     api("com.electronwill.night-config:json:3.9.0")
 }
+
+tasks.jar {
+    manifest.attributes (
+        "Main-Class" to "org.lushplugins.pluginupdater.cli.PluginUpdaterCLI"
+    )
+}
